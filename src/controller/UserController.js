ec60762abdca37d808dc651a91cf098ac873c946
Eduard-747 <88149958+Eduard-747@users.noreply.github.com>
2022-07-06 13:47:55 +0400

    Update UserController.js

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,7 +1,7 @@
 
 // Standart requires
 const router = require('express').Router();
-const StatusCode = require('http-status-codes').StatusCodes;
+const { StatusCode } = require('http-status-codes');
 
 // Library requires
 
